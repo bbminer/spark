e844b60950216b126d22d704a282fb74d981690c
PC-20170304HXPM\Administrator <654279133@qq.com>
2017-09-04 14:51:13 +0800

    .

--- src/main/scala/com/min/max/ShareVar.scala
+++ src/main/scala/com/min/max/ShareVar.scala
@@ -7,7 +7,8 @@ object ShareVar {
   val sc = new SparkContext(conf)
 
   def main(args: Array[String]): Unit = {
-    accumulatorTest()
+    //    accumulatorTest()
+    broadCastTest()
   }
 
   def accumulatorTest(): Unit = {
@@ -19,5 +20,18 @@ object ShareVar {
       println("bcount")
     }).collect() //不加 .collect() 累加器的值为0
     println("bcount:" + bcount)
+  }
+
+  //广播
+  def broadCastTest(): Unit = {
+    //广播因为做缓存处理，所以读取的表比较小，后面多次使用，就不用去库里拿
+    val test = sc.broadcast[List[(Int, String)]](List((1, "name1"), (2, "name2"), (3, "name3"), (4, "name4")))
+    val file = sc.textFile("src\\main\\scala\\com\\min\\max\\test")
+    val result = file.map(f => f.split(" ")).map(x => {
+      val name = test.value.filter(p => p._1 == x(0).toInt)
+      val r = if (name.size > 0) name(0)._2 else "no name"
+      (x, r)
+    }).map(y => y._2 :: y._1.toList).collect()
+    result.foreach(println)
   }
 }
